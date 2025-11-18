a750a44dff7e3e93228bc075706c7c9d588569d6
giraudc - cpu <giraudcgc@gmail.com>
2025-11-18 22:36:42 +0100

    Lecture 05_filtering

---
+++ 05_filtering/sql/more_filters.sql
@@ -0,0 +1,13 @@
+-- In 
+SELECT DISTINCT COUNT(*)
+FROM data_jobs WHERE company_size IN ('M','S');
+
+--FILTER clause
+SELECT 
+COUNT(*) AS total_jobs,
+COUNT(*) FILTER (remote_ratio = 100) AS remote_jobs,
+ROUND(remote_jobs/total_jobs*100) AS percentage_remote_jobs,
+FROM data_jobs;
+
+-- OFFSET filtering
+SELECT * FROM data_jobs OFFSET 6;
